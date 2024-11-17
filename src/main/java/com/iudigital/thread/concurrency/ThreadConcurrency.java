d1986bb69f549829128a68e5b88c73805439a0cf
osminPeña <osminv91@gamil.com>
2024-11-17 14:43:26 -0500

    Iniciando repository de thread-concurrecy

---
+++ src/main/java/com/iudigital/thread/concurrency/ThreadConcurrency.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.iudigital.thread.concurrency;
+
+/**
+ *
+ * @author HP 255-G9
+ */
+public class ThreadConcurrency {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
